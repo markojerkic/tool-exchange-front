4fe2a9395a8a5b0397482e8fe52c0ef08fca4644
Marko Jerkić <marko.jerkic@fer.hr>
2021-10-19 23:13:21 +0200

    add p-grid and p-fluid for form group

--- src/components/Login.js
+++ src/components/Login.js
@@ -19,29 +19,30 @@ class Login extends Component{
   render(){
     
     return(
-      <div>
+      <div className="card">
         <div className="p-d-flex p-jc-center">
           <h2>Dobrodošli!</h2>
         </div>
-        <div className="p-d-flex p-jc-center">
-          <div>
-            <span className="p-float-label">
-              <InputText id="username" value={this.state.username} onChange={(e) => this.setState({username: e.target.value})} />
-              <label htmlFor="username">Username</label>
-            </span>
+        <div className="p-grid p-fluid">
+          <div className="p-col-12 p-d-flex p-jc-center">
+            <div className="p-field">
+              <span className="p-float-label">
+                <InputText id="username" value={this.state.username} onChange={(e) => this.setState({username: e.target.value})} />
+                <label htmlFor="username">Username</label>
+              </span>
+            </div>
           </div>
+
+          <div className="p-col-12 p-d-flex p-jc-center">
+            <div className="p-field">
+              <span className="p-float-label">
+                <Password value={this.state.password} onChange={(e) => this.setState({ password: e.target.value })} toggleMask />
+                <label htmlFor="password">Password</label>
+              </span>
+            </div>
+          </div>
+
         </div>
-
-        <br />
-
-        <div className="p-d-flex p-jc-center">
-            <span className="p-float-label">
-              <Password value={this.state.password} onChange={(e) => this.setState({ password: e.target.value })} toggleMask />
-              <label htmlFor="password">Password</label>
-            </span>
-        </div>
-
-        <br />
 
         <div className="p-d-flex p-jc-center">
           <Button label="Login" className="p-button-raised p-button-rounded" />
