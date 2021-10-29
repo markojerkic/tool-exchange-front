117b880b69d5445d749c832f9f7217fdb3d3e2ee
Ivan Gusić <ivan.gusic@fer.hr>
2021-10-29 14:32:29 +0200

    Error 404 page design

--- src/components/error/Error404.js
+++ src/components/error/Error404.js
@@ -1,11 +1,13 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 
 const Home = () => {
 
 
-    return(
+    return (
         <div>
-            <h1 className="stdText">ERROR 404 PAGE NOT FOUND</h1>
+            <h1 className="p-text-center" style={{ fontSize: "9.5rem", marginBottom: "0" }}>404</h1>
+            <h4 className="p-text-center" style={{ fontSize: "2.5rem", marginTop: "0", marginBottom: "2%" }}>Page not found</h4>
+            <p className="p-text-center" style={{ fontSize: "1.3rem", marginTop: "2%" }} > The requested URL {window.location.pathname} was not found on this server.</p>
         </div>
     )
 }
