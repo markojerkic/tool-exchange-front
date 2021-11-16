a2643003da819e1bd2468dc017f868e17e7f1609
Ivan Gusić <ivan.gusic@fer.hr>
2021-11-16 15:21:09 +0100

    Prevedena stranica na hrvatski

--- src/components/error/Error404.js
+++ src/components/error/Error404.js
@@ -6,8 +6,11 @@ const Home = () => {
     return (
         <div>
             <h1 className="p-text-center" style={{ fontSize: "9.5rem", marginBottom: "0" }}>404</h1>
-            <h4 className="p-text-center" style={{ fontSize: "2.5rem", marginTop: "0", marginBottom: "2%" }}>Page not found</h4>
-            <p className="p-text-center" style={{ fontSize: "1.3rem", marginTop: "2%" }} > The requested URL {window.location.pathname} was not found on this server.</p>
+            <h4 className="p-text-center" style={{ fontSize: "2.5rem", marginTop: "0", marginBottom: "2%" }}>Ups! Nešto se dogodilo.</h4>
+            <p className="p-text-center" style={{ fontSize: "1.3rem", marginTop: "2%" }} > Pokušavate pristupiti stranici {window.location.pathname} koja nije
+                pronađena na serveru. <br />
+                Možete se vratiti nazad ili posjetiti početnu stranicu.
+            </p>
         </div>
     )
 }
