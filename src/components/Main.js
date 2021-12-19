685c350a62a9dc75a15b477737e394241663a01b
Mislav Matić <mislavmatichr@gmail.com>
2021-12-19 22:16:05 +0100

    Pregled oglasa i zahtjeva
    Nije povezano s backend-om, za sad su tu placeholderi

--- src/components/Main.js
+++ src/components/Main.js
@@ -10,6 +10,8 @@ import Error404 from "./error/Error404";
 import NewAd from "./new-ad/NewTool";
 import Category from "./new-ad/Category"
 import NewRequest from "./new-ad/NewRequest";
+import AdView from "./AdView";
+import ReqView from "./ReqView";
 
 const Main = () => {
     return (
@@ -22,6 +24,8 @@ const Main = () => {
                 <PrivateRoute exact path="/new-ad" component={Category} />
                 <PrivateRoute exact path="/new-ad/tool" component={NewAd} />
                 <PrivateRoute exact path="/new-ad/request" component={NewRequest} />
+                <PrivateRoute exact path="/ad/:id" component={AdView} />
+                <PublicRoute restricted={false} exact path="/req/:id" component={ReqView} />
                 <PublicRoute component={Error404} />
             </Switch>
         </main>
