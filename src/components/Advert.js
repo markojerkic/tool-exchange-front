abda5c8f44391bef8bd52930fe5a8f35075d5c79
Marko Jerkić <marko.jerkic@fer.hr>
2021-12-21 18:38:26 +0100

    fix list

--- src/components/Advert.js
+++ src/components/Advert.js
@@ -1,21 +1,19 @@
-import React, { useState } from 'react';
-import { Steps } from 'primereact/steps';
+import React, {useState} from 'react';
 
 const Advert = props => {
-    const [advert, setAdvert] = useState(props.singleAdvert);
-
+    const [advert] = useState(props.ad);
+    console.log(advert)
 
     return(
 
         <div className="singleAdvert grid-parent">
             <div className="grid-child-element">
-            <img src="default_picture.jpg" className="advertPicture"></img>
+            <img src="default_picture.jpg" className="advertPicture" />
             </div>
             <div className="grid-child-element">
-                <p className="advertElement advertTitle">{advert.name}</p>
-                <p className="advertElement desc">{advert.desc}</p>
-                <span className="advertDate">{advert.date}</span>
-                <p className="advertLocation">{advert.location}</p>
+                <p className="advertElement advertTitle">{advert.title}</p>
+                <p className="advertElement desc">{advert.details}</p>
+                <span className="advertDate">{advert.lastModified}</span>
             </div>
         </div>
     );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,8 @@
-import React, {useContext, useEffect, useState} from "react";
-import {AuthContext} from "../common/auth.context";
+import React, {useEffect, useState} from "react";
 import AdService from '../service/ads/ad.service';
 import Advert from "./Advert";
 
 const Home = () => {
-    const {user} = useContext(AuthContext);
     const [ads, setAds] = useState([]);
 
     useEffect(() => {
@@ -13,15 +11,13 @@ const Home = () => {
         })
     }, []);
 
-    const loggedInUser = !!user? <p>Trenutno je prijavljen korisnik s korisničkim imenom <b>{user.username}</b></p>: <p>Trenutno nije prijavljen korisnik</p>;;
-
     return(
         <div className="mainView shape">
             <div className="adView shape home-tag">
                 <h1 className="title">Search results</h1>
                 {
-                    ads.forEach(singleAdvert => {
-                        <Advert ad={singleAdvert}/>
+                    ads.map(singleAdvert => {
+                        return <Advert key={singleAdvert.id} ad={singleAdvert}/>
                     })
                 }
             </div>
