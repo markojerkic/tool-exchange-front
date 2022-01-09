11291c1e91cfb6b632dc1b6b6f269b5a96f48ffa
Filip Vulama <fv52381@fer.hr>
2022-01-09 19:42:46 +0100

    Novi home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,35 @@
 import React from "react";
-import AdvertList from "./advert/AdvertList";
-import RequestList from "./request/RequestList";
 import {Card} from "primereact/card";
+import { useState } from "react";
+import { useEffect } from "react";
+import {useHistory} from 'react-router-dom';
+
+function getWindowDimensions() {
+	const { innerWidth: width, innerHeight: height } = window;
+	return {
+	  width,
+	  height
+	};
+  }
+  
+  export function useWindowDimensions() {
+	const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+  
+	useEffect(() => {
+	  function handleResize() {
+		setWindowDimensions(getWindowDimensions());
+	  }
+  
+	  window.addEventListener('resize', handleResize);
+	  return () => window.removeEventListener('resize', handleResize);
+	}, []);
+  
+	return windowDimensions;
+  }
 
 const Home = () => {
+	const history = useHistory();
+	const {width, height} = useWindowDimensions();
 	return (
 		// <div className="mainView shape">
 			<Card className="ad-container">
@@ -14,11 +40,40 @@ const Home = () => {
 					</div>
 
 					<div className="lg:col-8 md:col-8 sm:col-12">
-						<h1 className="title">Rezultati pretrage</h1>
-						<h2>Alati</h2>
-						<AdvertList/>
-						<h2>Zahtjevi</h2>
-						<RequestList/>
+						<h1 className="title p-mb-5" >Odaberite kategoriju</h1>
+						{width > 700 &&
+						<div className="p-grid center">
+							<div className="p-col-4">
+								<h3>Forum</h3>
+								<i className="pi pi-users buttonHome" onClick={() => history.push('/forum') }></i>
+							</div>
+							<div className="p-col-4">
+								<h3>Alati</h3>
+								<i className="pi pi-briefcase buttonHome" onClick={() => history.push('/tools')}></i>
+							</div>
+							<div className="p-col-4">
+								<h3>Zahtjevi</h3>
+								<i className="pi pi-bookmark buttonHome" onClick={() => history.push('/requests')}></i>
+							</div>
+						</div>
+						}
+
+						{width < 700 &&
+						<div className="p-grid center">
+							<div className="p-col-12">
+								<h3>Forum</h3>
+								<i className="pi pi-users buttonHome" onClick={() => history.push('/forum') }></i>
+							</div>
+							<div className="p-col-12">
+								<h3>Alati</h3>
+								<i className="pi pi-briefcase buttonHome" onClick={() => history.push('/tools')}></i>
+							</div>
+							<div className="p-col-12">
+								<h3>Zahtjevi</h3>
+								<i className="pi pi-bookmark buttonHome" onClick={() => history.push('/requests')}></i>
+							</div>
+						</div>
+						}
 					</div>
 
 					<div className="lg:col-2 md:col-2 sm:col-12"/>
