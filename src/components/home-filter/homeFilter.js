ab75f618f1de7a3b8f61c539ed372f8a109034c6
lvanlvan <ivan.gusic@fer.hr>
2022-01-11 02:36:05 +0100

    dodan slider za range

--- src/components/home-filter/homeFilter.js
+++ src/components/home-filter/homeFilter.js
@@ -17,14 +17,14 @@ import { InputSwitch } from 'primereact/inputswitch';
 import '../new-entry/fade-animation.css';
 import Stepper from '../stepper/Stepper'
 import FilterService from "../../service/filter/filter.service";
-
+import { Divider } from 'primereact/divider';
 
 
 const HomeFilter = () => {
 
     const defaultValues = {
         titleContains: '',
-        maxRange: 30,
+        maxRange: 40,
         electric: null,
         hasBattery: null,
         power: [0, 5000],
@@ -37,7 +37,6 @@ const HomeFilter = () => {
         { name: 'Neispravno/Oštećeno', code: 'DAMAGED' }
     ];
 
-
     const [loading, setLoading] = useState(false);
     const { control, formState: { errors }, handleSubmit, reset, setValue } = useForm({ defaultValues });
     const history = useHistory();
@@ -49,7 +48,7 @@ const HomeFilter = () => {
 
         let filters = {
             titleContains: data.titleContains,
-            maxRange: data.maxRange,
+            maxRange: data.maxRange == 0 ? 1 : data.maxRange == 10 ? 2 : data.maxRange == 20 ? 5 : data.maxRange == 30 ? 10 : data.maxRange == 40 ? 20 : data.maxRange == 50 ? 50 : data.maxRange == 60 ? 100 : data.maxRange == 70 ? 200 : 10000,
             condition: data.condition,
             electric: data.electric,
             power: data.power,
@@ -70,96 +69,95 @@ const HomeFilter = () => {
     return (
 
         <Card className="card-container" title="Filtriraj rezultate">
-            <form onSubmit={handleSubmit(onSubmit)} className="grid p-fluid p-formgrid form-layout">
 
-                <div>
-                    <span className="p-float-label">
-                        <Controller name="titleContains" control={control}
+            <div className="p-grid p-dir-col">
+
+                <form onSubmit={handleSubmit(onSubmit)} className="p-grid p-dir-col">
+
+                    <div className="p-col">
+                        <span className="p-float-label">
+                            <Controller name="titleContains" control={control}
+                                render={({ field, fieldState }) => (
+                                    <InputText id={field.titleContains} {...field}
+
+                                        className={classNames({ 'p-invalid': fieldState.invalid })}
+                                        type="text" />
+                                )} />
+                            <label htmlFor="titleContains" className={classNames({ 'p-error': errors.title })}>Pretraga po nazivu ili opisu</label>
+                        </span>
+                    </div>
+                    <div className="p-col">
+
+                        <Controller name="maxRange" control={control} defaultValue={defaultValues.maxRange}
                             render={({ field, fieldState }) => (
-                                <InputText id={field.titleContains} {...field}
-                                    size={30}
-                                    className={classNames({ 'p-invalid': fieldState.invalid })}
-                                    type="text" />
+
+                                <div>
+                                    <h5>Radijus pretrage: {field.value == 0 ? "1 km" : field.value == 10 ? "2 km" : field.value == 20 ? "5 km" : field.value == 30 ? "10 km" : field.value == 40 ? "20 km" : field.value == 50 ? "50 km" : field.value == 60 ? "100 km" : field.value == 70 ? "200 km" : "Sve"} </h5>
+                                    <Slider value={field.value} onChange={(e) => field.onChange(e.value)} min={0} max={80} step={10} />
+                                </div>
                             )} />
-                        <label htmlFor="titleContains" className={classNames({ 'p-error': errors.title })}>Pretraga po nazivu ili opisu</label>
-                    </span>
-                </div>
+                    </div>
+                    <div className="p-col">
 
-                <div className="p-field col-15 md:col-6 lg:col-6 sm:col-12">
-                    <h5>
-                        Radijus pretrage:
-                    </h5>
-                    <InputNumber value={defaultValues.maxRange}
-                        size={5}
-                        suffix=" km"
-                    />
-                </div>
-
-                <div className="p-field col-15 md:col-6 lg:col-6 sm:col-12"></div>
-
-                <div className="p-field col-15 md:col-6 lg:col-6 sm:col-12">
-
-                    <Controller name="electric" control={control}
-                        render={({ field, fieldState }) => (
-                            <InputSwitch id={field.name} inputId={field.name}
-                                onChange={(e) => {
-                                    field.onChange(e.value);
-                                    setElectric(e.value);
-                                }}
-                                checked={field.value}
-                                className={classNames({ 'p-invalid': fieldState.invalid })} />
-                        )} />
-                    <label className='col' htmlFor="electric">Električan</label>
-                </div>
-
-                {electric &&
-
-                    <div className="grid p-dir-col col-12 md:col-8 lg:col-8 sm:col-8" id="animDiv">
-                        <div className="p-field col-15 md:col-6 lg:col-6 sm:col-12">
-
-                            <Controller name="power" control={control} defaultValue={defaultValues.power}
-                                render={({ field, fieldState }) => (
-
-                                    <div>
-                                        <h5>Snaga: {field.value[0]} W - {field.value[1]} W</h5>
-                                        <Slider value={field.value} onChange={(e) => field.onChange(e.value)} range min={0} max={5000} step={100} />
-                                    </div>
-                                )} />
-                        </div>
-                        <div className='p-field grid p-dir-col ml-2'>
-                            <Controller name="hasBattery" control={control} className='col'
-                                render={({ field, fieldState }) => (
-                                    <InputSwitch id={field.name} inputId={field.name}
-                                        onChange={(e) => field.onChange(e.value)}
-                                        checked={field.value}
-                                        className={classNames({ 'p-invalid': fieldState.invalid })} />
-                                )} />
-                            <label className='col' htmlFor="hasBattery">Ima bateriju</label>
-                        </div>
+                        <Controller name="electric" control={control}
+                            render={({ field, fieldState }) => (
+                                <InputSwitch id={field.name} inputId={field.name}
+                                    onChange={(e) => {
+                                        field.onChange(e.value);
+                                        setElectric(e.value);
+                                    }}
+                                    checked={field.value}
+                                    className={classNames({ 'p-invalid': fieldState.invalid })} />
+                            )} />
+                        <label className='col' htmlFor="electric">Električan</label>
                     </div>
 
-                }
+                    {electric &&
 
-                <div className="p-field col-15 md:col-6 lg:col-6 sm:col-12"></div>
+                        <div className="p-col" id="animDiv">
+                            <div className="p-col">
+                                <Controller name="power" control={control} defaultValue={defaultValues.power}
+                                    render={({ field, fieldState }) => (
 
-                <div className="p-field col-15 md:col-6 lg:col-6 sm:col-12">
-                    <Controller name="condition" control={control} defaultValue={defaultValues.condition}
-                        render={({ field, fieldState }) => (
-                            <div>
-                                <h5>Stanje alata</h5>
-                                <MultiSelect value={field.value} options={initialConditions} maxSelectedLabels={2} selectedItemsLabel=" Ozanačena 3 stanja" onChange={(e) => field.onChange(e.value)} optionLabel="name" placeholder="Stanje" />
+                                        <div>
+                                            <h5>Snaga: {field.value[0]} W - {field.value[1] == 5000 ? "max" : field.value[1] + " W"} </h5>
+                                            <Slider value={field.value} onChange={(e) => field.onChange(e.value)} range min={0} max={5000} step={100} />
+                                        </div>
+                                    )} />
                             </div>
-                        )} />
-                </div>
-                <div className="col-12 flex justify-content-center">
-                    <div>
-                        <Button type="submit" label="Filtriraj" className="mt-2"
-                            loading={loading} />
+                            <div className="p-col">
+                                <Controller name="hasBattery" control={control} className='col'
+                                    render={({ field, fieldState }) => (
+                                        <InputSwitch id={field.name} inputId={field.name}
+                                            onChange={(e) => field.onChange(e.value)}
+                                            checked={field.value}
+                                            className={classNames({ 'p-invalid': fieldState.invalid })} />
+                                    )} />
+                                <label className='col' htmlFor="hasBattery">Ima bateriju</label>
+                            </div>
+                        </div>
+                    }
+
+
+                    <div className="p-col">
+                        <Controller name="condition" control={control} defaultValue={defaultValues.condition}
+                            render={({ field, fieldState }) => (
+                                <div>
+                                    <h5>Stanje alata</h5>
+                                    <Dropdown value={field.value} options={initialConditions} onChange={(e) => field.onChange(e.value)} showClear optionLabel="name" placeholder="Stanje" />
+                                </div>
+                            )} />
+                    </div>
+                    <div className="p-col justify-content-center">
+                        <div>
+                            <Button type="submit" label="Filtriraj" className="mt-2"
+                                loading={loading} />
+                        </div>
+
                     </div>
 
-                </div>
-
-            </form>
+                </form>
+            </div>
 
         </Card>
 
